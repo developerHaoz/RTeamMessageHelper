ef520dad591dafb7cff2cf1c2f9df5e91e94d510
Haoz <developerhaon@gmail.com>
2017-11-12 00:42:12 +0800

    选择的逻辑完成。

--- app/src/main/java/com/developerhaoz/rteammessagehelper/MainActivity.java
+++ app/src/main/java/com/developerhaoz/rteammessagehelper/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<ContactBean> mContactBeanList = new ArrayList<>();
     private List<ContactBean> mCheckContactList = new ArrayList<>();
-    private Map<Integer, Integer> mPositionMap = new HashMap<>();
+    private Map<Integer, Boolean> mCheckMap = new HashMap<>();
 
     private RecyclerView mRvContacts;
 
@@ -41,12 +39,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void check(int position, boolean isCheck) {
                 if(isCheck){
-                    mCheckContactList.add(mContactBeanList.get(position));
-                    mPositionMap.put(position, mCheckContactList.size());
+                    mCheckMap.put(position, true);
                 }else{
-                    Toast.makeText(MainActivity.this, mPositionMap.get(position) + "", Toast.LENGTH_SHORT).show();
-                    mCheckContactList.remove(mPositionMap.get(position));
-                    mPositionMap.remove(position);
+                    mCheckMap.put(position, false);
                 }
             }
         });
@@ -58,10 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "hello world");
-                for (ContactBean contact : mCheckContactList) {
-                    Log.d(TAG, "onClick: " + contact.getName());
-                }
             }
         });
     }
@@ -70,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
         mContactBeanList = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             mContactBeanList.add(new ContactBean("15626144073", "李泽浩" + i));
+            mCheckMap.put(i, false);
         }
     }
+
+
 }
